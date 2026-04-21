ac5a6bf268e0895d2ef06a50906a4e98a608fcc8
frashsym <rfqfrashsym@gmail.com>
2026-04-21 13:29:13 +0700

    belajar layouting

---
+++ Web 1/code/new-calle/script.js
@@ -0,0 +1,11 @@
+function loadComponent(id, file) {
+    fetch(file)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(id).innerHTML = data;
+        });
+}
+
+loadComponent("header", "header.html");
+loadComponent("detail", "detail.html");
+loadComponent("footer", "footer.html");
