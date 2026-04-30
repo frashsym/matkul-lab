faff6a7f45ea15cecdd966ee872f005f30e74b2c
frashsym <rfqfrashsym@gmail.com>
2026-04-30 08:49:49 +0700

    test sebelom uts

---
+++ Alpro 2/code/nitip pretest biya/script.js
@@ -0,0 +1,10 @@
+function loadComponent(id, file) {
+    fetch(file)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(id).innerHTML = data;
+        });
+}
+loadComponent("header", "header.html");
+loadComponent("rinci_produk", "rinci_produk.html");
+loadComponent("footer", "footer.html");
